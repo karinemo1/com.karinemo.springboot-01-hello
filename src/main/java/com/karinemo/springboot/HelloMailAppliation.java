3081ceb837ab291cc6a1432aa24b19986300d4de
karinemo1 <1145945821@qq.com>
2020-08-30 13:37:57 +0800

    springboot快速启动

---
+++ src/main/java/com/karinemo/springboot/HelloMailAppliation.java
@@ -0,0 +1,12 @@
+package com.karinemo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMailAppliation {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMailAppliation.class, args);
+    }
+
+}
